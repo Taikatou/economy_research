4257206f5479cd034a219cc15636c69f4e9ab2b8
Pierre <pierre.galus@live.fr>
2021-02-12 13:36:05 +0100

    Reset
    Reset active agents/shop system/request system/battle system

--- Assets/EconomyProject/EnvironmentReset.cs
+++ Assets/EconomyProject/EnvironmentReset.cs
@@ -1,23 +1,106 @@
-﻿using EconomyProject.Scripts.MLAgents.AdventurerAgents;
+﻿using EconomyProject.Monobehaviours;
+using EconomyProject.Scripts.MLAgents.AdventurerAgents;
 using EconomyProject.Scripts.MLAgents.Shop;
+using EconomyProject.Scripts.UI;
 using UnityEngine;
 
 public class EnvironmentReset : MonoBehaviour
 {
-    public GameObject adventurerAgent;
-    public GameObject shopAgent;
-    public void ResetScript()
-    {
-        var agents = adventurerAgent.GetComponentsInChildren<AdventurerAgent>();
-        foreach (var agent in agents)
-        {
-            agent.ResetEconomyAgent();
-        }
-        
-        var shopAgents = shopAgent.GetComponentsInChildren<ShopAgent>();
-        foreach (var agent in shopAgents)
-        {
-            agent.ResetEconomyAgent();
-        }
-    }
+	protected GetCurrentAdventurerAgent getCurrentAdventurerAgent;
+	protected GetCurrentShopAgent getCurrentShopAgent;
+	public ShopCraftingSystemBehaviour shopCraftingSystemBehaviour;
+	public RequestShopSystemBehaviour requestShopSystemBehaviour;
+	public RequestAdventurerSystemBehaviour requestAdventurerSystemBehaviour;
+	public AdventurerSystemBehaviour adventurerSystemBehaviour;
+
+	public GameObject UIBlocker;
+	public GameObject ConfigUI;
+
+	void Start()
+	{
+		getCurrentAdventurerAgent = GameObject.FindObjectOfType<GetCurrentAdventurerAgent>();
+		getCurrentShopAgent = GameObject.FindObjectOfType<GetCurrentShopAgent>();
+	}
+
+	public void ResetScript()
+	{
+		//TO TEST
+		Debug.Log("RESET");
+
+		UIBlocker.SetActive(true);
+		ConfigUI.SetActive(true);
+
+		ResetBattle();
+		ResetShop();
+		ResetRequest();
+		ResetAgents();
+		GoBackToMainMenu();
+	}
+
+	/// <summary>
+	/// Reset battles
+	/// </summary>
+	public void ResetBattle()
+	{
+		adventurerSystemBehaviour.system.ResetAdventurerSystem();
+	}
+
+	/// <summary>
+	/// Reset all the shop requests
+	/// </summary>
+	public void ResetShop()
+	{
+		shopCraftingSystemBehaviour.system.shopSubSubSystem.ResetShop();
+		shopCraftingSystemBehaviour.system.craftingSubSubSystem.ResetCraftingSubSystem();
+
+		shopCraftingSystemBehaviour.system.shopSubSubSystem.Refresh();
+	}
+
+	/// <summary>
+	/// Reset all the resource requests taken and the resource requests made
+	/// </summary>
+	public void ResetRequest()
+	{
+		requestAdventurerSystemBehaviour.system.requestSystem.ResetRequestSystem();
+		requestShopSystemBehaviour.system.requestSystem.ResetRequestSystem();
+
+		requestAdventurerSystemBehaviour.system.requestSystem.craftingRequestRecord.ResetCraftingRequestRecord();
+		requestShopSystemBehaviour.system.requestSystem.craftingRequestRecord.ResetCraftingRequestRecord();
+
+		requestAdventurerSystemBehaviour.system.requestSystem.Refresh();
+		requestShopSystemBehaviour.system.requestSystem.Refresh();
+	}
+
+	/// <summary>
+	/// Reset wallets + inventories of all agents
+	/// </summary>
+	public void ResetAgents()
+	{
+		foreach (var agent in getCurrentAdventurerAgent.GetAgents)
+		{
+			agent.ResetEconomyAgent();
+		}
+
+		foreach (var agent in getCurrentShopAgent.GetAgents)
+		{
+			agent.ResetEconomyAgent();
+		}
+	}
+
+	/// <summary>
+	/// Get all the agents to the main menu
+	/// </summary>
+	public void GoBackToMainMenu()
+	{
+		foreach (var agent in getCurrentAdventurerAgent.GetAgents)
+		{
+			//Not working if the adventurer is in a battle
+			//agent.SetAction(EAdventurerAgentChoices.MainMenu);
+		}
+
+		foreach (var agent in getCurrentShopAgent.GetAgents)
+		{
+			agent.SetAction(EShopAgentChoices.MainMenu);
+		}
+	}
 }
