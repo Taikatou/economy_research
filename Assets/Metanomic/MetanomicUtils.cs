37e69b9bcaed4c52bb49cdc5702bc27ac07cf1d7
Taikatou <conorstephens@gmail.com>
2022-10-03 14:17:40 -0700

    metanomic sdk

---
+++ Assets/Metanomic/MetanomicUtils.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Metanomic
+{
+    public class MetanomicUtils : MonoBehaviour
+    {
+        private string api_key = "4e4e9c61-7bc6-4c38-b67c-701382d1a33e";
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
